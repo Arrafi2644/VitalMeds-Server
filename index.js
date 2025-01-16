2ca6b5cf3405628ee1af5b722fad1248d548f554
Arrafi2644 <raffian2644@gmail.com>
2025-01-17 00:28:57 +0600

    Insert new user to users collection

--- index.js
+++ index.js
@@ -25,9 +25,18 @@ async function run() {
 
     const userCollection = client.db("VitalMeds").collection("Users");
 
+    app.get('/users', async(req, res) => {
+        const result = await userCollection.find().toArray()
+        res.send(result)
+    })
 
     app.post('/users', async(req, res) => {
         const user = req.body;
+        const query = {email: user.email}
+        const isExist = await userCollection.findOne(query);
+        if(isExist){
+           return res.send({message: 'already-exist'});
+        }
         const result = await userCollection.insertOne(user)
         res.send(result)
     })
