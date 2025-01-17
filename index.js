acae1cd21156fa357c43ef1fcd9786d84fdea966
Arrafi2644 <raffian2644@gmail.com>
2025-01-17 21:03:47 +0600

    Create jwt token, verify token and verify admin

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ require('dotenv').config()
 const express = require('express');
 const cors = require('cors');
 const app = express();
+const jwt = require('jsonwebtoken')
 const port = process.env.PORT || 5000;
 
 app.use(cors())
@@ -25,27 +26,69 @@ async function run() {
 
     const userCollection = client.db("VitalMeds").collection("Users");
 
-    app.get('/users', async(req, res) => {
-        const result = await userCollection.find().toArray()
-        res.send(result)
+    // verify token middleware 
+    const verifyToken = (req, res, next) => {
+      console.log("Inside the verify middleware ", req.headers.authorization);
+
+      if (!req.headers.authorization) {
+        return res.status(401).send({ message: 'unauthorized access' })
+      }
+
+      const token = req.headers.authorization.split(" ")[1];
+      console.log("Token is ", token);
+
+      jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, function (error, decoded) {
+        if (error) {
+          return res.status(401).send({ message: "unauthorized access" })
+        }
+        req.decoded = decoded;
+        next();
+      })
+
+    }
+
+    // verify admin 
+    const verifyAdmin = async(req, res, next) => {
+      const email = req.decoded.email;
+      const query = {email : email}
+      const user = await userCollection.findOne(query)
+      if(user.role !== 'admin'){
+        return res.status(403).send({message: "forbidden access"})
+      }
+      next()
+    }
+
+    // jwt related api 
+    app.post('/jwt', async (req, res) => {
+      const user = req.body;
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '12h' })
+      res.send({ token })
     })
 
-    app.delete('/users/:id', async(req, res)=> {
+    // users related api 
+
+    app.get('/users', verifyToken, verifyAdmin, async (req, res) => {
+      console.log(req.headers.authorization);
+      const result = await userCollection.find().toArray()
+      res.send(result)
+    })
+
+    app.delete('/users/:id', async (req, res) => {
       const id = req.params.id;
-      const query = {_id : new ObjectId(id)}
+      const query = { _id: new ObjectId(id) }
       const result = await userCollection.deleteOne(query)
       res.send(result)
     })
 
-    app.post('/users', async(req, res) => {
-        const user = req.body;
-        const query = {email: user.email}
-        const isExist = await userCollection.findOne(query);
-        if(isExist){
-           return res.send({message: 'already-exist'});
-        }
-        const result = await userCollection.insertOne(user)
-        res.send(result)
+    app.post('/users', async (req, res) => {
+      const user = req.body;
+      const query = { email: user.email }
+      const isExist = await userCollection.findOne(query);
+      if (isExist) {
+        return res.send({ message: 'already-exist' });
+      }
+      const result = await userCollection.insertOne(user)
+      res.send(result)
     })
 
     // Connect the client to the server	(optional starting in v4.7)
@@ -61,11 +104,11 @@ async function run() {
 run().catch(console.dir);
 
 app.get('/', (req, res) => {
-    res.send("VitalMeds is running")
+  res.send("VitalMeds is running")
 })
 
-app.listen(port, ()=>{
-    console.log("VitalMeds is running on port ", port);
+app.listen(port, () => {
+  console.log("VitalMeds is running on port ", port);
 })
 
 
