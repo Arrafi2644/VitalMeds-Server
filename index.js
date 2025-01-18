281b530636420cc67bb0ec5d0ecd5ea50a31ca09
Arrafi2644 <raffian2644@gmail.com>
2025-01-19 00:05:46 +0600

    Complete medicine add to cart

--- index.js
+++ index.js
@@ -164,7 +164,13 @@ async function run() {
       res.send(result)
    })
 
+  //  Cart related api 
 
+    app.post('/carts', async(req, res) => {
+      const medicine = req.body;
+      const result = await cartCollection.insertOne(medicine)
+      res.send(result)
+    })
 
 
     
